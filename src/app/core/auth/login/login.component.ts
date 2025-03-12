2c8708a6fd1e2ad0121ae2b88f87067a9406736c
User <jithinraj9895@gmail.com>
2025-03-12 13:10:43 +0530

    initial commit

---
+++ src/app/core/auth/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ApiService } from '../../services/api.service';
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  loginForm:FormGroup;
+  constructor(private fb: FormBuilder,private api:ApiService) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.api.login(this.loginForm.value).subscribe(
+        response => {
+          console.log(response);
+        },
+        error=>{
+          alert("invalid");
+        }
+      )
+    }
+  }
+
+
+}
