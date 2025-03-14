60320c8b406f9aa329f1783493e369fa2cd141c4
User <jithinraj9895@gmail.com>
2025-03-14 12:35:30 +0530

    Added feed

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -1,14 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  private apiUrl = "http://localhost:8080/"
+  private apiUrl = "http://localhost:8080/";
 
   constructor(private http:HttpClient) { }
+
+  getIdeas(): Observable<any> {
+    return this.http.get(this.apiUrl+"ideas").pipe(
+      catchError((error) => {
+        console.error("Error fetching ideas:", error);
+        return throwError(() => new Error("Failed to fetch ideas"));
+      })
+    );
+  }
+  
 
 }
--- src/app/features/feed/feed.component.ts
+++ src/app/features/feed/feed.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../../core/services/api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-feed',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './feed.component.html',
   styleUrl: './feed.component.css'
 })
 export class FeedComponent {
+
+  public ideas: any[] = [];
+
+  constructor(private apiService: ApiService){}
+
+  ngOnInit(){
+    this.apiService.getIdeas().subscribe({
+      next:(response)=>{
+        this.ideas = response;
+      },
+      error:(err)=>{
+        "errrrprr";
+      }
+    })
+  }
 
 }
